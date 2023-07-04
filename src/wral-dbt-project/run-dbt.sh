c41712f883cc9e7d48507e80c0be5efb43821bae
Arumugam <arumugam.s@tringapps.com>
2023-07-04 17:05:09 +0530

    updated run-dbt.sh file

--- src/wral-dbt-project/run-dbt.sh
+++ src/wral-dbt-project/run-dbt.sh
@@ -6,7 +6,9 @@ echo "This job creates a database using a seed and then runs the dbt job"
 if [ $# -eq 0 ]; then
     echo "No models were specified. Executing all models"
     dbt run --profiles-dir .
+    dbt test
 else
     echo "Executing only specified models"
     dbt run --profiles-dir . -m $@
+    dbt test
 fi
